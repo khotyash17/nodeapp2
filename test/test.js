69821fd803e74dbd2360bfa4d755d3f683f94731
yashk17 <ubuntu@ip-172-31-17-112.ec2.internal>
2025-04-23 12:09:46 +0000

    testing wala

---
+++ test/test.js
@@ -0,0 +1,35 @@
+const request = require('supertest');
+
+const app = require('../app.js'); // Adjust if your app filename is app.js
+
+
+describe('GET /nonexistentpage', function() {
+
+  it('should respond with 404 Not Found', function(done) {
+
+    request(app)
+
+      .get('/nonexistentpage')
+
+      .expect(404, done); // This is a cleaner way to end test if only checking status
+
+  });
+
+});1~const request = require('supertest');
+
+const app = require('../app.js'); // Adjust if your app filename is app.js
+
+
+describe('GET /nonexistentpage', function() {
+
+  it('should respond with 404 Not Found', function(done) {
+
+    request(app)
+
+      .get('/nonexistentpage')
+
+      .expect(404, done); // This is a cleaner way to end test if only checking status
+
+  });
+
+});
